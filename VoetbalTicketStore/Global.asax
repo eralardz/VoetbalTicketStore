74ce8049c9c62695095b620ad27660c16307be91
Laurens De Wispelaere <laurens.dewispelaere@student.vives.be>
2018-04-01 04:20:25 +0200

    Add project files.

---
+++ VoetbalTicketStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VoetbalTicketStore.MvcApplication" Language="C#" %>
---
+++ VoetbalTicketStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VoetbalTicketStore.Startup))]
+namespace VoetbalTicketStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
