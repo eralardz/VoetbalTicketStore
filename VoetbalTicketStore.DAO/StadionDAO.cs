4c9431c8b1955fb69d1b3d5648b378c1ed5de0b1
Laurens De Wispelaere <laurens.dewispelaere@student.vives.be>
2018-04-01 05:29:16 +0200

    added layers

---
+++ VoetbalTicketStore.DAO/StadionDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoetbalTicketStore.Models;
+
+namespace VoetbalTicketStore.DAO
+{
+    public class StadionDAO
+    {
+        public IEnumerable<Stadion> All()
+        {
+            var db = new VoetbalEntities();
+
+            return db.Stadions; // lazy-loading
+        }
+    }
+}
---
+++ VoetbalTicketStore.Service/StadionService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoetbalTicketStore.DAO;
+using VoetbalTicketStore.Models;
+
+namespace VoetbalTicketStore.Service
+{
+    public class StadionService
+    {
+        private StadionDAO stadionDAO;
+
+        public StadionService()
+        {
+            stadionDAO = new StadionDAO();
+        }
+
+        public IEnumerable<Stadion> All()
+        {
+            return stadionDAO.All();
+        }
+    }
+}
