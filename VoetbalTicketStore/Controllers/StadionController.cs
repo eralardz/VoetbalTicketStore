000c31e7f9b998f8102b6b372d258220b5ef815a
Laurens De Wispelaere <laurens.dewispelaere@student.vives.be>
2018-04-03 07:02:54 +0200

    added ClubDAO + club images

---
+++ VoetbalTicketStore/Controllers/StadionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VoetbalTicketStore.Service;
+
+namespace VoetbalTicketStore.Controllers
+{
+    public class StadionController : Controller
+    {
+
+        private StadionService stadionService;
+
+        // GET: Stadion
+        public ActionResult Index()
+        {
+            stadionService = new StadionService();
+            var stadions = stadionService.All();
+            // we geven de lijst stadions mee aan de view
+            return View(stadions);
+        }
+    }
+}
