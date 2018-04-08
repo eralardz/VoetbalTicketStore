245d994a2cf53f6c5c99f8404437d512d994e41f
Laurens De Wispelaere <r0389268@student.vives.be>
2018-04-08 18:36:21 +0200

    added wedstrijdkalender

---
+++ VoetbalTicketStore.DAO/WedstrijdDAO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoetbalTicketStore.Models;
+
+namespace VoetbalTicketStore.DAO
+{
+    public class WedstrijdDAO
+    {
+        public IEnumerable<Wedstrijd> All()
+        {
+            //In general, if you're going to use the result of a query more than once, it's always a good idea to store it via ToList() or ToArray(). 
+            //This is especially true if you're LINQ query is an "expensive" one, as it prevents the expensive operation from running more than once.
+            var db = new VoetbalEntities();
+            return db.Wedstrijds.ToList(); // lazy-loading
+        }
+
+        public IEnumerable<Wedstrijd> getWedstrijdKalenderVanPloeg(Club club)
+        {
+            using (var db = new VoetbalEntities())
+            {
+                // standard .NET boolean operators are supported in a where clause
+                return db.Wedstrijds.Where(w => w.Club1id == club.id || w.Club2id == club.id).ToList();
+            }
+        }
+    }
+}
---
+++ VoetbalTicketStore.Service/WedstrijdService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoetbalTicketStore.DAO;
+using VoetbalTicketStore.Models;
+
+namespace VoetbalTicketStore.Service
+{
+    public class WedstrijdService
+    {
+        private WedstrijdDAO wedstrijdDAO;
+
+        public WedstrijdService()
+        {
+            wedstrijdDAO = new WedstrijdDAO();
+        }
+
+        public IEnumerable<Wedstrijd> All()
+        {
+            return wedstrijdDAO.All();
+        }
+
+
+        public IEnumerable<Wedstrijd> getWedstrijdKalenderVanPloeg(Club club)
+        {
+            return wedstrijdDAO.getWedstrijdKalenderVanPloeg(club);
+        }
+
+    }
+}
