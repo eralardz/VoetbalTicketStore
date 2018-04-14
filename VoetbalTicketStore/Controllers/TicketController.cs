b81d2afe5b8c7a2ef24a57ceb4c0e7783567bed0
Laurens De Wispelaere <laurens.dewispelaere@student.vives.be>
2018-04-14 18:29:13 +0200

    - passed id to ticket buying form

--- VoetbalTicketStore/Controllers/TicketController.cs
+++ VoetbalTicketStore/Controllers/TicketController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VoetbalTicketStore.Models;
+using VoetbalTicketStore.Service;
 
 namespace VoetbalTicketStore.Controllers
 {
     public class TicketController : Controller
     {
+
+        private TicketService ticketService;
+             
         // GET: Ticket
-        public ActionResult Index()
+        public ActionResult Buy(int id)
         {
+            ViewBag.WedstrijdId = id;
             return View();
         }
     }
