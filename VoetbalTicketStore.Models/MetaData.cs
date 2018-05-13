1f2f367b58d468530aff272284c141938e813de9
Laurens De Wispelaere <laurens.dewispelaere@student.vives.be>
2018-05-13 10:28:12 +0200

    data annotations via metadata + partial classes

---
+++ VoetbalTicketStore.Models/MetaData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoetbalTicketStore.Models
+{
+
+    public class BezoekerMetadata
+    {
+        [StringLength(11)]
+        [Display(Name = "Rijksregisternummer")]
+        public string Rijksregisternummer;
+
+        // TODO REGEX email
+        [StringLength(50)]
+        [Display(Name = "E-mailadres")]
+        public string Email;
+
+        [StringLength(50)]
+        [Display(Name = "Middle Name")]
+        public string Naam;
+
+        [StringLength(50)]
+        [Display(Name = "Voornaam")]
+        public string Voornaam;
+    }
+
+}
---
+++ VoetbalTicketStore.Models/PartialClasses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoetbalTicketStore.Models
+{
+    public class PartialClasses
+    {
+        [MetadataType(typeof(BezoekerMetadata))]
+        public partial class Bezoeker
+        {
+        }
+    }
+}
