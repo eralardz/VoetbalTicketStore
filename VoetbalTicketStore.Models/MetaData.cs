6ffaebd28bcd1ea8be07c8625e6782b6c647d121
Laurens De Wispelaere <laurens.dewispelaere@student.vives.be>
2018-05-14 18:57:47 +0200

    email regex

--- VoetbalTicketStore.Models/MetaData.cs
+++ VoetbalTicketStore.Models/MetaData.cs
@@ -10,19 +10,24 @@ namespace VoetbalTicketStore.Models
 
     public class BezoekerMetadata
     {
+        [Required]
         [StringLength(11)]
+        // TODO: hier culture ingeven? Met constants ofzo.
         [Display(Name = "Rijksregisternummer")]
         public string Rijksregisternummer;
 
-        // TODO REGEX email
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Dit is geen correct gevormd e-mailadres!")]
+        [Required]
         [StringLength(50)]
         [Display(Name = "E-mailadres")]
         public string Email;
 
+        [Required]
         [StringLength(50)]
-        [Display(Name = "Middle Name")]
+        [Display(Name = "Familienaam")]
         public string Naam;
 
+        [Required]
         [StringLength(50)]
         [Display(Name = "Voornaam")]
         public string Voornaam;
