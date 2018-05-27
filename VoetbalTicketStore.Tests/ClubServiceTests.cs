52f98e4bc988a0e992c292c758ff4ad01cbea4ff
Laurens De Wispelaere <r0389268@student.vives.be>
2018-05-27 21:45:50 +0200

    unit test PoC

---
+++ VoetbalTicketStore.Tests/ClubServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VoetbalTicketStore.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoetbalTicketStore.Models;
+using NUnit.Framework;
+
+// Tests moeten lopen op een testdatabank met officieel aangeleverde dummy data!
+namespace VoetbalTicketStore.Service.Tests
+{
+    [TestClass()]
+    public class ClubServiceTests
+    {
+
+        private ClubService clubService;
+
+
+        public ClubServiceTests()
+        {
+            clubService = new ClubService();
+        }
+
+        [TestMethod()]
+        public void AllTest()
+        {
+            IEnumerable<Club> clubs = clubService.All();
+
+            NUnit.Framework.Assert.That(clubs, Has.Count.EqualTo(6));
+        }
+    }
+}
---
+++ VoetbalTicketStore.Tests/Controllers/ClubControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VoetbalTicketStore.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoetbalTicketStore.Controllers.Tests
+{
+    [TestClass()]
+    public class ClubControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
